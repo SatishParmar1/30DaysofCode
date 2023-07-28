24b87d97fef23f1095c815b45b4d839a8ba5d46a
Satish Parmar <Satish Parmar1>
2023-07-29 04:37:54 +0530

    day6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Navbar from './component/Navbar';
+import Textform from './component/Textform';
+import Alert from './component/Alert';
+//import About from './component/About';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [mode, setMode] = useState('light');
+  const [state, setState] = useState('Enable Dark mode');
+  const[alert, setAlert] = useState(null);
+
+
+  const showAlert = (type,message) =>{
+    setAlert({
+      type:type,
+      message:message,
+   })
+  
+  setTimeout(() => {
+    setAlert(null);
+  }, 3000); 
+  }
+  const space = () => {
+
+  if (mode === 'light') {
+    setMode('dark');
+   setState('Enable light mode');
+   showAlert('success','Dark mode has been Enable');
+    document.body.style.background='rgb(17 24 39)';
+    document.title="30DAYSCODE-DARK";
+  } else {
+    setMode('light');
+    setState('Enable Dark mode');
+    showAlert('success','Light mode has been Enable');
+    document.body.style.background='white';
+    document.title="30DAYSCODE";
+  }
+  }
+
+  return (  
+    <>
+   
+    <Navbar Home="Home"  foryou="Foryou" mode={mode}  switch={space} hello={state} />
+    <Alert alert={alert}/>
+   
+    <div className="container my-3">
     </div>
+    <Textform showAlert={showAlert} hadding="Enter Your Text" mode={mode} />
+    </>
   );
 }
 
---
+++ src/component/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="fixed-width container my-5" id ='aboutcontainer'>
+    <form>
+  <div className="form-group">
+    <label htmlFor="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div className="form-group form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-outline-success my-3">Submit</button>
+</form>
+</div>
+    </>
+  )
+}
